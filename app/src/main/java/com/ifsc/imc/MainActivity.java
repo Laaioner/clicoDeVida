2bed0df3c39b444e658a79bd6c9099521f17dbd9
Aluno <pedropiovesan2002@gmail.com>
2025-04-10 21:05:21 -0300

    Paint que muda de cor ao clicar no botão cor

--- app/src/main/java/com/ifsc/imc/MainActivity.java
+++ app/src/main/java/com/ifsc/imc/MainActivity.java
@@ -1,16 +1,29 @@
 package com.ifsc.imc;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.skydoves.colorpickerview.ColorEnvelope;
+import com.skydoves.colorpickerview.ColorPickerDialog;
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button bntlimpa;
+    SimplePaint simplePaint;
+
+    Button buttonColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +31,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("clicodevida", "OnCreate");
         Toast.makeText(this,"OnCreate", Toast.LENGTH_LONG).show();
+        bntlimpa = findViewById(R.id.bntlimpa);
+        simplePaint = findViewById(R.id.simplePaint);
+        buttonColor = findViewById(R.id.buttonColor);
 
+
+        buttonColor.setOnClickListener(v -> {
+            new ColorPickerDialog.Builder(this)
+                    .setTitle("ColorPicker Dialog")
+                    .setPreferenceName("MyColorPickerDialog")
+                    .setPositiveButton("Confirma",
+                            new ColorEnvelopeListener() {
+                                @Override
+                                public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
+                                    v.setBackgroundColor(envelope.getColor());
+                                    simplePaint.changeColor(envelope.getColor());
+                                }
+                            })
+                    .setNegativeButton("Cancela",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                    .attachAlphaSlideBar(true) // the default value is true.
+                    .attachBrightnessSlideBar(true)  // the default value is true.
+                    .setBottomSpace(12) // set a bottom space between the last slidebar and buttons.
+                    .show();
+
+        });
+        bntlimpa.setOnClickListener( v -> {
+            simplePaint.clearDreaw();
+        });
     }
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -68,4 +112,5 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"onDestroy", Toast.LENGTH_LONG).show();
 
     }
+
 }
